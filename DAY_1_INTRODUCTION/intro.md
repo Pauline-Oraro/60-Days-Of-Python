55ce3e7beb01071dbce61e93d45e1e78dc9f1218
Pauline-Oraro <hiltrapauline@gmail.com>
2026-04-01 22:58:30 +0300

    Day-1-Introduction

---
+++ DAY_1_INTRODUCTION/intro.md
@@ -0,0 +1,13 @@
+# Introduction to python programming
+
+This section introduces the basics of Python, including how to display output
+
+---
+
+## What you will learn
+
+- How to use the `print()` function -print() displays output
+- How Python handles numbers -Python supports numbers and calculations
+- String concatenation - Use str() when combining numbers with text
+
+---
---
+++ DAY_1_INTRODUCTION/intro.py
@@ -0,0 +1,20 @@
+# Introduction to Python Programming
+# print functionnis used to display text or output values.
+
+print("hello world")
+print("welcome to python programming")
+print("python is a great language to learn")
+print("learing python is fun")
+
+#numbers
+print(3)
+print(111)
+print(1.45)
+print(4+5)
+print(10-6)
+print(4*5)
+print(9/3)
+
+#concatenation
+print("I am a " + "student")
+print("I am a " + str(20) + " year old student")
